7dfeedd5d3a5e0d1a5516b336d59a557c3672f5c
tzmfreedom <makoto_tajitsu@hotmail.co.jp>
2019-03-04 22:00:05 +0900

    impl todo app

--- warp-app/app/Main.hs
+++ warp-app/app/Main.hs
@@ -10,34 +10,64 @@ import Data.ByteString.Lazy as BSL
 import Data.ByteString.Builder
 import Data.Map as M
 import Data.Maybe
+import Data.IORef
+import Data.List as L
+import Data.List.Split
+import Codec.Binary.UTF8.String as CBS
+import Control.Monad
+import Control.Monad.Trans
+import Control.Applicative
 
 main :: IO ()
-main = run 8080 server
+main = do
+  tasks <- defaultTasks
+  run 8080 (server tasks)
 
-server :: Application
-server req respond = do
-  respond $ routes (rawPathInfo req) (requestMethod req) req
+defaultTasks :: IO (IORef [String])
+defaultTasks = newIORef ["foo", "bar", "baz"]
 
-routes :: BS.ByteString -> Method -> (Request -> Response)
-routes path method
-       | path == "/" && method == "GET"       = showIndex
-       | path == "/hello" && method == "POST" = showHoi
-       | path == "/req" && method == "GET"   = showRequest
-       | otherwise                          = notFound
+server :: IORef [String] -> Application
+server tasks req respond = do
+  let path = rawPathInfo req
+      paths = splitOn "/" (CBS.decode . BS.unpack $ BS.drop 1 path)
+      handler = getHandler paths
+  respond =<< handler req tasks
 
-notFound :: Request -> Response
-notFound _ = responseLBS status404 [] "Not Found"
+getHandler :: [String] -> Request -> IORef [String] -> IO Response
+getHandler (path:rest) = case path of
+       ""         -> showIndex
+       "hello"    -> showHoi
+       "add"      -> addTask
+       "delete"   -> deleteTask
+       _        -> notFound
 
-showIndex :: Request -> Response
-showIndex _ = responseFile status200 [] "./index1.html" Nothing
+notFound :: Request -> IORef a -> IO Response
+notFound _ _ = return $ responseLBS status404 [] "Not Found"
 
-showHoi :: Request -> Response
-showHoi _ = responseLBS status200 [] (toLazyByteString $ stringUtf8 "ほい！\n")
+showIndex :: Request -> IORef a -> IO Response
+showIndex _ _ = return $ responseFile status200 [] "./index1.html" Nothing
 
-showRequest :: Request -> Response
-showRequest req = do
+showHoi :: Request -> IORef a -> IO Response
+showHoi _ _ = return $ responseLBS status200 [] (toLazyByteString $ stringUtf8 "ほい！\n")
+
+addTask :: Request -> IORef [String] -> IO Response
+addTask req ref = do
   let kv = (queryString req !! 0)
       key = fst kv
       value = snd kv
-    in responseLBS status200 [] $ fromStrict $ key `BS.append` "=" `BS.append` (fromJust value) `BS.append` "\n"
+  tasks <- readIORef ref
+  let newTask = key `BS.append` "=" `BS.append` (fromJust value)
+      newTasks = (CBS.decode $ BS.unpack newTask):tasks
+  writeIORef ref newTasks
+  return (responseLBS status200 [] $ toLazyByteString $ stringUtf8 $ show newTasks)
+--  return (responseLBS status200 [] $ fromStrict $ key `BS.append` "=" `BS.append` (fromJust value) `BS.append` "\n")
 
+deleteTask :: Request -> IORef [String] -> IO Response
+deleteTask req ref = do
+  tasks <- readIORef ref
+  writeIORef ref (L.drop 1 tasks)
+  tasks' <- readIORef ref
+  let kv = (queryString req !! 0)
+      key = fst kv
+      value = snd kv
+  return (responseLBS status200 [] $ toLazyByteString $ stringUtf8 $ show tasks')
