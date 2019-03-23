6b9068143ac0823b6c7c735b59e81bfbb50b5c39
tzmfreedom <makoto_tajitsu@hotmail.co.jp>
2019-03-23 23:08:17 +0900

    update

--- blog-generator/app/Main.hs
+++ blog-generator/app/Main.hs
@@ -8,6 +8,7 @@ import System.Directory
 import System.Posix.Time
 import System.Posix.Files
 import System.Posix.Types
+import System.Environment
 import Foreign.C.Types
 import Control.Monad
 import Control.Monad.Trans(lift)
@@ -22,22 +23,52 @@ import Data.Yaml (decodeFileEither)
 import Data.Aeson as JSON
 import Data.Aeson.TH (deriveJSON, defaultOptions, Options(..))
 import Data.List as L
+import Data.Time.Format
+import GHC.Exts
 import Lib
 
 data Meta = Meta{
   metaTitle :: String,
   metaDescription :: String,
-  metaKeywords :: [String]
+  metaKeywords :: [String],
+  metaSrc :: String,
+  metaCreatedAt :: String
 } deriving Show
 
 main :: IO ()
 main = do
---  print =<< epochTime
+  args <- getArgs
+  case if L.length(args) == 0 then "generate" else args !! 0 of
+    "generate" -> generate
+    "index" -> generateIndex
+    "new" -> newBlog args
+    otherwise -> print $ "No such subcommand" ++ args !! 0
+  return ()
+
+generate :: IO ()
+generate = do
   files <- findUnprocessedSource
-  processFiles files
+  meta <- getMetaInfo files
+  processFiles meta
   current <- epochTime
   writeFile lastUpdatedFile $ show current
-  return ()
+
+generateIndex :: IO ()
+generateIndex = do
+  files <- findAllSource
+  meta <- getMetaInfo files
+  writeFile "dest/index.html" =<< renderIndex meta
+
+newBlog :: [String] -> IO ()
+newBlog args = do
+  let fname = srcDirectory ++ "/new.md"
+  let mname = metaDirectory ++ "/new.yml"
+  let title = if L.length(args) == 2 then args !! 1 else "new article"
+  doesExist <- doesFileExist fname
+  if not doesExist then do
+    writeFile fname $ "# " ++ title
+    writeFile mname $ "description:\nkeywords: []\ntitle: " ++ title
+    else return ()
 
 lastUpdatedFile :: FilePath
 lastUpdatedFile = "lastUpdated"
@@ -45,21 +76,29 @@ lastUpdatedFile = "lastUpdated"
 srcDirectory :: FilePath
 srcDirectory = "blogs"
 
+metaDirectory :: FilePath
+metaDirectory = "meta"
+
 layoutFile :: String
 layoutFile = "layout.html"
 
 lastUpdated :: IO String
 lastUpdated = readFile lastUpdatedFile
 
+findAllSource :: IO [FilePath]
+findAllSource = do
+  files <- listDirectory srcDirectory
+  return $ Prelude.map (\f -> srcDirectory ++ "/" ++ f) files
+
 findUnprocessedSource :: IO [FilePath]
 findUnprocessedSource = do
   files <- listDirectory srcDirectory
-  toList $ findUnprocessedSource' (Prelude.map (\f -> srcDirectory ++ "/" ++ f) files)
+  ListT.toList $ findUnprocessedSource' (Prelude.map (\f -> srcDirectory ++ "/" ++ f) files)
 
 findUnprocessedSource' :: [FilePath] -> ListT IO FilePath
 findUnprocessedSource' files = do
   f <- fromFoldable files
-  lift $ print f
+--  lift $ print f
   continue <- lift (shouldProcessed f)
   guard continue
   return f
@@ -75,19 +114,25 @@ shouldProcessed f = do
 -- get layout html
 -- Markdown => HTML
 -- write HTML
-processFiles :: [FilePath] -> IO [()]
-processFiles files = do
+processFiles :: [Meta] -> IO [()]
+processFiles metaInfo = do
+  (flip mapM) metaInfo $ \m -> do
+    let src = metaSrc m
+    content <- readFile src
+    html <- generateHTML (render content) m
+    writeFile (destFile src) html
+
+getMetaInfo:: [FilePath] -> IO [Meta]
+getMetaInfo files = do
   (flip mapM) files $ \f -> do
-    content <- readFile f
     meta <- decodeFileEither $ metaFile f
-    html <- generateHTML (render content) $ getMeta meta
-    writeFile (destFile f) html
+    return $ getMeta meta
 
 getMeta :: Either a Meta -> Meta
 getMeta (Right a) = a
 
 metaFile :: FilePath -> String
-metaFile f = "meta/" ++ (Prelude.drop (Prelude.length srcDirectory) (replaceExtension f ".yml"))
+metaFile f = metaDirectory ++ "/" ++ (Prelude.drop (Prelude.length srcDirectory) (replaceExtension f ".yml"))
 
 destFile :: FilePath -> String
 destFile f = "dest/" ++ (Prelude.drop (Prelude.length srcDirectory) (replaceExtension f ".html"))
@@ -105,9 +150,11 @@ generateHTML content meta = do
 parser :: String -> Meta -> Parser String
 parser src meta = do
   ls <- many $ (
-    (noneOf "{" >>= (\c -> return [c])) <|>
     try (string "{{" *> spaces *> string "body" *> spaces *> string "}}" *> return src) <|>
-    try (string "{{" *> spaces *> string "meta" *> spaces *> string "}}" *> return (renderMeta meta))
+    try (string "{{" *> spaces *> string "meta" *> spaces *> string "}}" *> return (renderMeta meta)) <|>
+    try (string "{{" *> spaces *> string "title" *> spaces *> string "}}" *> return ("<h1>" ++ metaTitle meta ++ "</h1>")) <|>
+    try (string "{{" *> spaces *> string "createdAt" *> spaces *> string "}}" *> return (metaCreatedAt meta)) <|>
+    (anyChar >>= (\c -> return [c]))
     )
   return $ Prelude.concat ls
 
@@ -119,5 +166,25 @@ renderMeta meta = do
     where
       keys :: String
       keys = L.intercalate "," $ metaKeywords meta
+
+renderIndex :: [Meta] -> IO String
+renderIndex metaInfo = do
+  layout <- readFile "index.html"
+  case parse p "" layout of
+    Left err -> error "ParseError"
+    Right html -> return html
+  where
+    p :: Parser String
+    p = do
+      parts <- many (
+        try (string "{{" *> spaces *> string "body" *> spaces *> string "}}" *> return lst) <|>
+        (anyChar >>= (\c -> return [c]))
+        )
+      return $ Prelude.concat parts
+    lst = L.foldl (\x m -> x ++ "<li>" ++ metaCreatedAt m ++ ": <a href=\"/" ++ metaToPath m ++ "\">" ++ metaTitle m ++ "</a></li>") "" sorted
+    sorted = sortWith (\m -> metaCreatedAt m) metaInfo
+
+metaToPath :: Meta -> String
+metaToPath meta = replaceExtension (takeFileName $ metaSrc meta) ".html"
 
 deriveJSON defaultOptions { fieldLabelModifier = firstLower . Prelude.drop 4 } ''Meta
